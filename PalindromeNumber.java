d898580b41d157f0a6580ea338fdaef93847ca80
sawyer7246 <1789199491@qq.com>
2015-07-16 13:17:23 +0800

    another palindrome solution
    this one is more efficient

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,6 +1,36 @@
 public class Solution {
+﻿public class Solution {
     public boolean isPalindrome(int x) {
         String num = String.valueOf(x);
         return  new StringBuffer(num).reverse().toString().equalsIgnoreCase(num);
     }
+
+
+    public boolean isPalindrome(int x) {
+        if(x<0)
+            return false;
+            
+        if(x == 0)
+            return true;
+        
+        //获取x的位数
+        int base = 1;
+        
+        while(x/base>=10){
+            base*=10;
+        }
+        
+        while(x!=0){
+            int leftDigit = x/base;
+            int rightDigit = x%10;
+            
+            if(leftDigit != rightDigit )
+                return false;
+            
+            //返回中间的数
+            x=(x%base)/10;
+            base/=100;
+        } 
+        return true;
+	}
 }
